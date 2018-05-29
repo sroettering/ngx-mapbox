ab5926cebc84248bf229aeacce0cab5eab6955c9
Sven Röttering <svenroettering@googlemail.com>
2018-05-29 22:17:07 +0200

    Put the TableOfContentsComponent into a side nav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { LAYERS_ROUTES } from './layers/layers.routes';
 import { STYLES_ROUTES } from './styles/styles.routes';
 import { TableOfContentsComponent } from './table-of-contents/table-of-contents.component';
+import { ApiComponent } from './api/api.component';
 
 export const APP_ROUTES: Routes = [
     {
         path: '',
-        component: TableOfContentsComponent,
+        component: ApiComponent,
     },
     {
         path: 'styles',
