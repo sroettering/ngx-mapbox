9ae2d2a4883d36eca35b9f9e24ede965cba870e2
Sven Röttering <sven.roettering@10m.de>
2018-06-14 16:09:13 +0200

    Export the ImageComponent

--- projects/ngx-mapbox/src/public_api.ts
+++ projects/ngx-mapbox/src/public_api.ts
@@ -5,4 +5,5 @@
 export * from './lib/access-token';
 export * from './lib/layer/layer.component';
 export * from './lib/map/map.component';
+export * from './lib/image/image.component';
 export * from './lib/ngx-mapbox.module';
